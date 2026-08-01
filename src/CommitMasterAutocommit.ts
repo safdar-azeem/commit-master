115f23a13993e7c4a8148ce21083a40a8462f580
safdar-azeem <safdarazeem00@gmail.com>
2026-08-01 23:59:29 +0500

    Update CommitMasterAutocommit.ts

--- src/CommitMasterAutocommit.ts
+++ src/CommitMasterAutocommit.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { runCli } from './CommitMasterCli.js';
+import { runCli } from './CommitMasterCli.js'
 
-await runCli('autocommit', process.argv.slice(2));
+await runCli('autocommit', process.argv.slice(2))
